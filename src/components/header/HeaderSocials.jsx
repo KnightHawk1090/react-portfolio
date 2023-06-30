ea0b373378855d290f48a1682280bb868821a96f
KnightHawk1090 <121210332+KnightHawk1090@users.noreply.github.com>
2023-06-29 22:06:51 -0500

    ts deployment issues

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import {BsLinkedin} from 'react-icons/bs'
-import {FaGithub} from 'react-icons/fa'
- 
+import React from 'react';
+import { BsLinkedin } from 'react-icons/bs';
+import { FaGithub } from 'react-icons/fa';
+
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href='https://www.linkedin.com/in/eric-partain-674787161/' target='_blank'><BsLinkedin/></a>
-        <a href='https://github.com/KnightHawk1090' target='_blank'><FaGithub/></a>
+      <a href='https://www.linkedin.com/in/eric-partain-674787161/' target='_blank' rel='noopener noreferrer'>
+        <BsLinkedin />
+      </a>
+      <a href='https://github.com/KnightHawk1090' target='_blank' rel='noopener noreferrer'>
+        <FaGithub />
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default HeaderSocials
+export default HeaderSocials;
